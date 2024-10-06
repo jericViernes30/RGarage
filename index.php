fd46968c81cf68ef166afdbe348e5511bf8f5879
Jeric James Viernes <jericviernes06@gmail.com>
2024-10-06 21:59:13 +0800

    added new routes

--- index.php
+++ index.php
@@ -4,10 +4,14 @@ include_once 'controllers/UserController.php';
 $controller = new UserController();
 $action = $_GET['action'] ?? null;
 
-if ($action === 'user/create-user') {
-    $controller->create();
+if ($action === 'user/auth/create') {
+    $controller->authLogin();
 } elseif ($action === 'index') {
     $controller->index();
+} elseif ($action === 'user/login') {
+    $controller->login();
+} elseif ($action === 'user/signup') {
+    $controller->signup();
 } else {
     include 'views/landing_page.php';
 }
