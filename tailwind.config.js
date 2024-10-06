df3479b5e549577c48b0f47d1a28360159ccaf73
Jeric James Viernes <jericviernes06@gmail.com>
2024-10-06 18:49:38 +0800

    added daisy ui

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./views/**/*.php",
+    "./views/*.php"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'black-v1': '#1b1c1e'
+      }
+    },
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+}
+
