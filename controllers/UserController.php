f38271e7eb54744fb802838f1771f6789d148378
Jeric James Viernes <jericviernes06@gmail.com>
2024-10-06 21:59:46 +0800

    added create user function

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -20,14 +20,43 @@ class UserController {
         include_once 'views/user/index.php';
     }
 
-    public function create() {
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $this->user->name = $_POST['name'];
-            $this->user->email = $_POST['email'];
-            $this->user->create();
-            header("Location: index.php");
-        } else {
-            include_once 'views/user/create.php';
+    public function authLogin(){
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+            $password = $_POST['password'];
+            $repeat_password = $_POST['repeat_password'];
+
+            if($password == $repeat_password){
+                $this->user->first_name = $_POST['first_name'];
+                $this->user->last_name = $_POST['last_name'];
+                $this->user->email_address = $_POST['email_address'];
+                $this->user->contact_number = $_POST['contact_number'];
+                $this->user->address = $_POST['address'];
+                $this->user->password = $_POST['password'];
+
+                // Attempt to create the user
+                if ($this->user->createUser()) {
+                    // Redirect or display success message
+                    echo "User registered successfully!";
+                } else {
+                    // Display error message
+                    echo "Error: Could not create user.";
+                }
+            } else {
+                echo "<script>
+                    alert('Error: Passwords do not match.');
+                    window.location.href = '/RGarage/user/signup';
+                </script>";
+                exit(); // Prevent further script execution
+            }
+
         }
+    }
+
+    public function login(){
+        include 'views/user/login.php';
+    }
+
+    public function signup(){
+        include 'views/user/signup.php';
     }
 }
