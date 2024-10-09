ff59ce73712c7185ec9473243d1864a3e89b971e
Jeric James Viernes <jericviernes06@gmail.com>
2024-10-09 20:30:47 +0800

    route for login and logout added

--- index.php
+++ index.php
@@ -4,15 +4,42 @@ include_once 'controllers/UserController.php';
 $controller = new UserController();
 $action = $_GET['action'] ?? null;
 
-if ($action === 'user/auth/create') {
-    $controller->authLogin();
-} elseif ($action === 'index') {
-    $controller->index();
-} elseif ($action === 'user/login') {
-    $controller->login();
-} elseif ($action === 'user/signup') {
-    $controller->signup();
-} else {
-    include 'views/landing_page.php';
+// Start the session if it hasn't been started yet
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
 }
+
+// Check if the user is logged in (session variable example)
+$isLoggedIn = isset($_SESSION['user']);
+
+switch ($action) {
+    case 'user/auth/create':
+        $controller->createUser();
+        break;
+    case 'user/login':
+        $controller->login();
+        break;
+    case 'user/signup':
+        $controller->signup();
+        break;
+    case 'user/auth/login':
+        $controller->authLogin();
+        break;
+    case 'user/home':
+        $controller->home();
+        break;
+    case 'user/logout':
+        if ($isLoggedIn) {
+            $controller->logout();
+        } else {
+            header("Location: /RGarage/user/login");
+            exit();
+        }
+        break;
+    default:
+        include 'views/landing_page.php';
+        break;
+}
+
+
 ?>
