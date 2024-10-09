677e4a55f1df1e0ffbab5e9ed63fb5202fd52c90
Jeric James Viernes <jericviernes06@gmail.com>
2024-10-09 20:31:23 +0800

    enhanced user authentication

--- models/User.php
+++ models/User.php
@@ -31,10 +31,73 @@ class User{
         return $stmt->execute();
     }
 
-    // Get all users
-    public function read() {
-        $query = "SELECT * FROM " . $this->table_name;
-        $result = $this->conn->query($query);
-        return $result;
+    // authenticate user if present in database
+    public function authUser() {
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
+        $query = "SELECT id, first_name, last_name, email_address, address, contact_number, password FROM " . $this->table_name . " WHERE email_address = ? LIMIT 1";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bind_param("s", $this->email_address);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        
+        if ($result->num_rows == 1) {
+            $row = $result->fetch_assoc();
+            if (password_verify($this->password, $row['password'])) {
+                return [
+                    'status' => true,
+                    'user' => [
+                        'id' => $row['id'],
+                        'first_name' => $row['first_name'],
+                        'last_name' => $row['last_name'],
+                        'email_address' => $row['email_address'],
+                        'address' => $row['address'],
+                        'contact_number' => $row['contact_number']
+                    ]
+                ];
+            } else {
+                return [
+                    'status' => false,
+                    'message' => 'Invalid password.'
+                ];
+            }
+        } else {
+            return [
+                'status' => false,
+                'message' => 'User not found.'
+            ];
+        }
     }
+
+    // Get all users
+    // public function getDetails($id) {
+    //     $query = "SELECT * FROM " . $this->table_name . " WHERE id = ?";
+    //     $stmt = $this->conn->prepare($query);
+    //     $stmt->bind_param('i', $id);
+    //     $stmt->execute();
+    //     $result = $stmt->get_result();
+
+    //     if($result->num_rows == 1){
+    //         $row = $result->fetch_assoc();
+
+    //         return [
+    //             'status' => true,
+    //             'message' => 'User details found!',
+    //             'user' => [
+    //                 'id' => $row['id'],
+    //                 'first_name' => $row['first_name'],
+    //                 'last_name' => $row['last_name'],
+    //                 'email_address' => $row['email_address'],
+    //                 'contact_number' => $row['contact_number'],
+    //                 'address' => $row['address'],
+    //             ],
+    //         ];
+    //     } else {
+    //         return [
+    //             'status' => false,
+    //             'message' => 'User details not found!',
+    //         ];
+    //     }
+    // }
 }
