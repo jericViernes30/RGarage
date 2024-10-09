3c6c191ec60da1e20c7e9d51dab99ea4a6b93961
Jeric James Viernes <jericviernes06@gmail.com>
2024-10-09 20:31:58 +0800

    the login and signup button will disappear if there's a user signed in

--- views/landing_page.php
+++ views/landing_page.php
@@ -6,7 +6,14 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
   <link href="./src/output.css" rel="stylesheet">
+  <style>
+    #profileDropdown{
+      --animate-duration: 0.3s;
+    }
+  </style>
 </head>
 <body class="w-full bg-white overflow-x-hidden">
   <div class="sticky top-0 left-0 z-50">
