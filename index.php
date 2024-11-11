901cdf34516e733bbebac882159a4731dac0cf4a
Jeric James Viernes <jericviernes06@gmail.com>
2024-11-11 14:06:48 +0800

    new routes for inquiring

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
 include_once 'controllers/UserController.php';
 include_once 'controllers/AdminController.php';
+include_once 'controllers/MessageController.php';
 
 $controller = new UserController();
 $adminController = new AdminController();
+$messageController = new MessageController();
 $action = $_GET['action'] ?? null;
 
 // Start the session if it hasn't been started yet
@@ -48,6 +50,13 @@ switch ($action) {
         }
         break;
 
+    case 'user/send-message':
+        $messageController->sendMessage();
+        break;
+
+    case 'user/messages/fetch':
+        $messageController->fetchMessages();
+        break;
 
     case 'admin/dashboard':
         $adminController->dashboard();
@@ -63,6 +72,22 @@ switch ($action) {
 
     case 'admin/delete-unit':
         $adminController->deleteUnit(); // Call deleteUnit without passing plate number directly
+        break;
+
+    case 'admin/messages':
+        $messageController->displaySenders();
+        break;
+
+    case 'admin/get-message':
+        $messageController->getMessagesBySenderName();
+        break;
+
+    case 'admin/send-message':
+        $messageController->sendAdminMessage();
+        break;
+
+    case 'admin/get-recent-messages':
+        $messageController->getRecentMessages();
         break;
         
 
