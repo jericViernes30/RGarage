03dedcdd7cf0507a686bbac6e57a54b035183251
Jeric James Viernes <jericviernes06@gmail.com>
2024-12-05 00:58:15 +0800

    fix styles

--- views/admin/messages.php
+++ views/admin/messages.php
@@ -53,8 +53,8 @@
                                 }
                             }
                 ?>
-                        <button type="button" class="senderNameBtn w-full flex flex-col gap-2 p-4 bg-[#575454] rounded-lg mb-1">
-                            <p class="font-bold"><?php echo htmlspecialchars($messages['sender_name']); ?></p>
+                        <button type="button" class="senderNameBtn w-full flex flex-col gap-2 p-4 bg-black-v1 rounded-lg mb-1">
+                            <p class="font-bold text-white"><?php echo htmlspecialchars($messages['sender_name']); ?></p>
                                 <div class="w-full flex gap-1 items-center">
                                 <p class="text-sm text-gray-400">
                         <?php
@@ -87,7 +87,7 @@ endforeach;
                 <div id="contents" class="w-11/12 overflow-y-scroll h-4/5 mx-auto py-4">
                 </div>
                 <div id="form" class="w-11/12 flex items-center mx-auto gap-4 py-4">
-                    <input type="text" id="messageInput" name="message" placeholder="Write your message here" class="px-4 py-3 w-11/12 outline-none bg-[#575454] rounded-xl text-sm text-black-v1">
+                    <input type="text" id="messageInput" name="message" placeholder="Write your message here" class="px-4 py-3 w-11/12 outline-none bg-black-v1 rounded-xl text-sm text-white">
                     <svg class="hover:cursor-pointer" id="sendButton" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="20" height="20" fill="#000"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M498.1 5.6c10.1 7 15.4 19.1 13.5 31.2l-64 416c-1.5 9.7-7.4 18.2-16 23s-18.9 5.4-28 1.6L284 427.7l-68.5 74.1c-8.9 9.7-22.9 12.9-35.2 8.1S160 493.2 160 480l0-83.6c0-4 1.5-7.8 4.2-10.8L331.8 202.8c5.8-6.3 5.6-16-.4-22s-15.7-6.4-22-.7L106 360.8 17.7 316.6C7.1 311.3 .3 300.7 0 288.9s5.9-22.8 16.1-28.7l448-256c10.7-6.1 23.9-5.5 34 1.4z"/></svg>
                 </div>
             </div>
@@ -170,8 +170,8 @@ $(document).on('click', '.senderNameBtn', function() {
                                 ${message.sender_name !== 'Admin' ? '' : 'text-right'}
                                 ${message.sender_name !== 'Admin' ? '' : 'justify-end'}
                                 ${message.sender_name !== 'Admin' ? '' : 'ml-auto'}
-                                ${message.sender_name !== 'Admin' ? 'bg-gray-200' : 'bg-blue-200'}">
-                                <p>${message.content}</p>
+                                ${message.sender_name !== 'Admin' ? 'bg-gray-200' : 'bg-gray-500'}">
+                                <p class="text-black-v1">${message.content}</p>
                                 <p class="text-xs text-gray-600">${message.created_at}</p>
                             </div>
                         `;
