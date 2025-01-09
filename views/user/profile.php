2fbb6d8c4ba1a534bf40dae689b78ddb70e68171
Jeric James Viernes <jericviernes06@gmail.com>
2025-01-09 17:49:34 +0800

    profile view

---
+++ views/user/profile.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.mi  n.css"/>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+  <link href="./src/output.css" rel="stylesheet">
+  <style>
+    #profileDropdown{
+      --animate-duration: 0.3s;
+    }
+  </style>
+</head>
+<body>
+    <div class="sticky top-0 left-0 z-50">
+        <?php
+            include($_SERVER['DOCUMENT_ROOT'] . '/RGarage/views/components/header.php');
+        ?>
+    </div>
+    <p>Profile View</p>
+</body>
+</html>
